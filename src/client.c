6d39d07d665db88837eb9dd0d132df6181ac6bd8
glenvollmer <glenvollmer@gmail.com>
2025-11-26 09:22:56 -0600

    ENHANCEMENT: code cleanup, project revival. (#2)
    * cleaned up logic, made server and client functional again
    
    * updated pid for child process type

--- src/client.c
+++ src/client.c
@@ -12,46 +12,13 @@ int connected;
 int connection_est = 1;
 pthread_t thread;
 
-// void *get_server_messages(void *arg)
-// {
-//     pthread_t id = pthread_self();
-//     int network_socket = *((int *) arg);
-//     int msg_status;
-//     char server_response[256];
- 	
-//     while (connected)
-// 	{
-// 		printf("test\n");
-
-// 		msg_status = recv(network_socket, 
-// 			&server_response, 
-// 			sizeof(server_response), 
-// 			0);
-
-// 		if (msg_status != -1)
-// 		{
-// 			printf("%s\n", server_response);
-// 		}
-
-// 		printf("wut\n");
-// 	}
-// 	printf("??????\n");
-    
-//     for(int i=0; i<(0xFFFFFFFF);i++);
-//     return NULL;
-// }
-
 void *get_server_messages(int network_socket)
 {
-    // pthread_t id = pthread_self();
-    // int network_socket = *((int *) arg);
     int msg_status;
     char server_response[256];
  	
     while (connected)
 	{
-		printf("test\n");
-
 		msg_status = recv(network_socket, 
 			&server_response, 
 			sizeof(server_response), 
@@ -59,25 +26,27 @@ void *get_server_messages(int network_socket)
 
 		if (msg_status != -1)
 		{
-			printf("%s\n", server_response);
+			printf("Server message: %s\n", server_response);
+		} else {
+			printf("There was an error recieving a message from the server!\n");
 		}
-
-		printf("wut\n");
-	}
-	printf("??????\n");
-    
-    // for(int i=0; i<(0xFFFFFFFF);i++);
+	}    
     return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
+	pid_t pid;
+	
 	struct sockaddr_in server_address;
+	
+	char user_input[256];
+	
 	int network_socket, 
-		connection_status, 
-		server_socket,
-		input_length,
-		pid;
+	connection_status, 
+	server_socket,
+	input_length,
+	input_status;
 	
 	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -85,20 +54,21 @@ int main(int argc, char const *argv[])
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY; // ip address of the socket.
 		
-	connection_status = connect(network_socket, 
+	connection_status = connect(
+		network_socket, 
 		(struct sockaddr*) &server_address,
-		sizeof(server_address));
+		sizeof(server_address)
+	);
 
 	if (connection_status == -1)
 	{
 		connected = 0;
-		printf("there was an error\n");
+		printf("There was an error connecting to the server!\n");
+		return 0;
 	}
 	else
 	{
-		connected = 1;
-		server_socket = accept(network_socket, NULL, NULL);
-
+		connected = 1;		
 		pid = fork();
 		if (pid == 0)
 		{
@@ -107,29 +77,15 @@ int main(int argc, char const *argv[])
 		}
 		else
 		{
-			printf("%d\n", pid);
 			while (connected)
 			{
-				sleep(2);
-				char user_input[256];
-				
-				// fork process for scanf();	
-						
-						// printf("a\n");   
+	    		input_status = scanf("%256s", user_input);
+				if (input_status < 0) {
+					printf("There was an error accepting client input!");
+					return 0;
+				}
 
-						// pthread_create(&thread, 
-						// 	NULL, 
-						// 	&get_server_messages, 
-						// 	&network_socket);
-
-						connection_est = 0;
-
-	    		
-	    		// printf("here\n");
-		    	// printf("sent\n\n");
-
-	    		// scanf("%256s", user_input);
-				send(server_socket, 
+				send(network_socket, 
 					user_input, 
 					sizeof(user_input), 
 					0);
--- src/server.c
+++ src/server.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -10,66 +7,83 @@
 
 int main(int argc, char const *argv[])
 {
+	// create socket
 	char server_connect_msg[] = "You have connected";
-	int server_socket, client_socket, msg_status;
+	char server_response[256];
+	int server_socket, socket_bind, socket_listen, client_socket, msg_status;
 	struct sockaddr_in server_address;
 	
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (server_socket == -1)
+	{
+		printf("Server socket failed to create!\n");
+		return 0;
+	}
+
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY; // ip address of the socket.
 
-	bind(server_socket, 
+	// bind socket to localhose:9002
+	socket_bind = bind(
+		server_socket, 
 		(struct sockaddr*) &server_address,
-		sizeof(server_address));
+		sizeof(server_address)
+	);
+
+	if (socket_bind == -1)
+	{
+		printf("Server socket failed to bind!\n");
+		return 0;
+	}
+
+	socket_listen = listen(server_socket, 5);
+	if (socket_listen == -1)
+	{
+		printf("Server socket failed to listen!\n");
+		return 0;
+	}
+	printf("Server running...\n");
+
+	client_socket = accept(
+		server_socket,
+		(struct sockaddr*)&server_address,
+		(socklen_t*)&server_address
+	);
+	if (client_socket == -1)
+	{
+		printf("Server socket failed to accept new connections!\n");
+		return 0;
+	}
+
+	printf("client socket accepted: %d\n", client_socket);
+	
+	send(
+		client_socket, 
+		server_connect_msg, 
+		sizeof(server_connect_msg), 
+		0
+	);
 
 	while(1) 
 	{
-		printf("1\n");
-		char server_response[256];
+		msg_status = read(
+			client_socket, 
+			server_response, 
+			sizeof(server_response) 
+		);
 		
-		printf("2\n");
-		listen(server_socket, 5);
-
-
-
-		client_socket = accept(server_socket, NULL, NULL);
-		// needs to fork the process for each connection.
-		printf("3\n");
-		
-		msg_status = recv(server_socket, 
-			&server_response, 
-			sizeof(server_response), 
-			0);
-
-		if(client_socket != -1)
-		{
-			printf("%d\n", client_socket);
-			send(client_socket, 
-				server_connect_msg, 
-				sizeof(server_connect_msg), 
-				0);
-		} 
-		else 
-		{
-			printf("No connections \n");
-		}
-		
-		printf("%d\n", msg_status);
-
 		if(msg_status != -1)
 		{
-			printf("\ntest\n");
-			printf("%s\n", server_response);
-			printf("\ntest\n");
-
-			printf("%s\n", server_response);
-			send(client_socket, 
+			printf("Client message: %s\n", server_response);
+			send(
+				client_socket, 
 				server_response, 
 				sizeof(server_response), 
-				0);
+				0
+			);
 		}
-			
 	}
 
 	close(server_socket);
